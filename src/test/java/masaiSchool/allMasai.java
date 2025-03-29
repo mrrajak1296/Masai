815a212ba104ead23322f7ed8438c016e4e4091b
PC <mrrajak1296@gmail.com>
2025-03-29 17:05:28 +0530

    first commit

---
+++ src/test/java/masaiSchool/allMasai.java
@@ -0,0 +1,91 @@
+package masaiSchool;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class allMasai {
+	WebDriver driver;
+
+	@BeforeClass
+	public void Url() {
+		driver =new ChromeDriver();
+	driver.get("https://www.masaischool.com/");
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	
+	@Test(priority=1)
+	public void homepage() {
+		
+		 WebElement logo = driver.findElement(By.cssSelector("[alt=\"masai Logo\"]"));
+	        assert logo.isDisplayed();
+		
+	}
+	
+	@Test (priority=2)
+	public void Regiser() {
+		
+		driver.findElement(By.xpath("//button[@id='nav_login']")).click();
+		driver.findElement(By.cssSelector("[placeholder=\"Enter full name\"]")).sendKeys("Anil Rajak");
+		driver.findElement(By.id("email")).sendKeys("mrrajak1296@gmail.com");
+		driver.findElement(By.id("phone_number")).sendKeys("9876543210");
+		driver.findElement(By.cssSelector("button[type='submit']")).click();
+		
+		
+	}
+	
+	@Test(priority=3)
+	public void Login() {
+		driver.findElement(By.xpath("//button[@id='nav_login']")).click();
+		driver.findElement(By.xpath("//button[normalize-space()='Sign in']")).click();
+		driver.findElement(By.id("emailOrPhone")).sendKeys("9876543210");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+	}
+	
+	@Test(priority=4)
+	public void Hirefromus() throws InterruptedException {
+		
+		driver.findElement(By.cssSelector("li:nth-child(3) a:nth-child(1)")).click();
+		driver.findElement(By.cssSelector("[placeholder=\"Enter your name\"]")).sendKeys("Anil");
+		driver.findElement(By.id("company")).sendKeys("Masai");
+		driver.findElement(By.id("email")).sendKeys("mrrajak1296@gmail.com");
+		driver.findElement(By.id("phone_number")).sendKeys("9876543210");
+		driver.findElement(By.xpath("//button[contains(@type,'submit')]")).click();
+		
+		
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement sdte = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//body//svelte//select[1]")));
+        sdte.click();
+        Select drop = new Select(sdte);
+        drop.selectByVisibleText("SDET");
+		
+		
+        WebElement Experience  = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//body//svelte//select[2]"))) ;
+		Experience.click();
+		Select drop1 = new Select(Experience);
+		drop1.deselectByValue("Entry Level (0-2 years)");
+		
+		WebElement Hiring = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//body//svelte//select[3]"))) ;
+		Hiring.click();
+		Select drop2 = new Select(Hiring);
+		drop1.deselectByValue("1 month");
+		
+		WebElement Location = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//body//svelte//select[4]")));
+		Location.click();
+		Select drop3 = new Select(Location);
+		drop1.deselectByValue("Delhi NCR");
+		
+	}
+	
+
+}
